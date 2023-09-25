87078164e46cb10537965ab0d9d19e0d93ecda79
AlloysiusM <vgw1434@autuni.ac.nz>
2023-09-25 20:29:41 +1300

    Front page handles login and sign up

---
+++ P17_GUI_Assignment/src/p17_gui_assignment/FrontPage.java
@@ -0,0 +1,21 @@
+package p17_gui_assignment;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Albrent Manlutac
+ */
+public class FrontPage {
+
+    public static void main(String[] args) {
+        // Create an instance of the Login class
+        Login loginFrame = new Login();
+
+        // Set it as visible
+        loginFrame.setVisible(true);
+    }
+}
