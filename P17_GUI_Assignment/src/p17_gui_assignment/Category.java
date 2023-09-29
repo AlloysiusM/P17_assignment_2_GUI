1a5d8e5c6225a1f6f0b5021780d4ef0e6b83033f
AlloysiusM <vgw1434@autuni.ac.nz>
2023-09-29 22:07:26 +1300

    category model

---
+++ P17_GUI_Assignment/src/p17_gui_assignment/Category.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p17_gui_assignment;
+
+/**
+ *
+ * @author Albrent Manlutac
+ */
+class Category {
+
+    private String name;
+    private String description;
+    private int id;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
