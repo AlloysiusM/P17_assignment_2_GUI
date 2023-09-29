fbe862bcc40ee49444e05b2c70b5bfe09122a990
AlloysiusM <vgw1434@autuni.ac.nz>
2023-09-29 22:09:55 +1300

    product Model

---
+++ P17_GUI_Assignment/src/p17_gui_assignment/Product.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p17_gui_assignment;
+
+/**
+ *
+ * @author Albrent Manlutac
+ */
+class Product extends Item {
+
+    public Product(String id, String name, double price, String productInfo, Category category) {
+        super(id, name, price, productInfo, category);
+    }
+}
