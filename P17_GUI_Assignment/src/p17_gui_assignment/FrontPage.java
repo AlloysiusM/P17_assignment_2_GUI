66a00ebf5c2fda9c23228ed9dc4f20f67feb52da
AlloysiusM <vgw1434@autuni.ac.nz>
2023-09-30 20:49:32 +1300

    Now inserting values to items and category database

--- P17_GUI_Assignment/src/p17_gui_assignment/FrontPage.java
+++ P17_GUI_Assignment/src/p17_gui_assignment/FrontPage.java
@@ -13,18 +13,41 @@ import java.sql.SQLException;
 public class FrontPage {
 
     public static void main(String[] args) throws SQLException {
-        
-        // Create database tables and insert initial data
-        CategoryDB categoryDB = new CategoryDB();
-        ItemDB itemDB = new ItemDB();
-        
-        categoryDB.createCategoriesTable();
-        itemDB.createItemsTable();
-        
         // Create an instance of the Login class
         Login loginFrame = new Login();
 
         // Set it as visible
         loginFrame.setVisible(true);
+
+        // Create database tables and insert initial data
+        CategoryDB categoryDB = new CategoryDB();
+        ItemDB itemDB = new ItemDB(categoryDB); // Pass the categoryDB instance to the ItemDB constructor
+
+        categoryDB.createCategoriesTable();
+        itemDB.createItemsTable();
+
+        // Create categories
+        Category electronics = new Category(1, "Electronics");
+        Category clothing = new Category(2, "Clothing");
+
+        // Insert categories into the database
+        categoryDB.insertCategory(electronics);
+        categoryDB.insertCategory(clothing);
+
+        // Create items
+        Item laptop = new Item("12345", "Laptop", 799.99, "High-performance laptop", electronics) {
+        };
+        Item smartphone = new Item("67890", "Smartphone", 499.99, "Flagship smartphone", electronics) {
+        };
+        Item shirt = new Item("54321", "T-Shirt", 19.99, "Cotton T-Shirt", clothing) {
+        };
+        Item jeans = new Item("98765", "Jeans", 39.99, "Blue Jeans", clothing) {
+        };
+
+        // Insert items into the database
+        itemDB.insertItem(laptop);
+        itemDB.insertItem(smartphone);
+        itemDB.insertItem(shirt);
+        itemDB.insertItem(jeans);
     }
 }
